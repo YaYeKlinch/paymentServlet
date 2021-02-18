19d43fa7630f54f78d776c640401fbe9421e49b2
YaYeKlinch <klisandrij@gmail.com>
2021-02-18 22:03:19 +0200

    empty security added

---
+++ src/main/java/com/example/payment/controller/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.payment.controller.config;
+
+import com.example.payment.entity.Role;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SecurityConfig {
+    private static final Map<String, List<String>> accessPaths = new HashMap<String, List<String>>();
+    static {
+        init();
+    }
+    private static void init(){
+        List<String> notSecuredPaths = new ArrayList<>();
+        notSecuredPaths.add("/login");
+        notSecuredPaths.add("/registration");
+        accessPaths.put("ANYONE",notSecuredPaths);
+
+        List<String> userPaths = new ArrayList<>(notSecuredPaths);
+        userPaths.add("/");
+        userPaths.add("/index.jsp");
+        userPaths.add("/logout");
+        accessPaths.put("USER",userPaths);
+
+        List<String>  adminPaths =   new ArrayList<>(userPaths);
+        accessPaths.put("ADMIN",adminPaths);
+    }
+    public static List<String> getPathsForRole(Role role){
+        return accessPaths.get(role.toString());
+    }
+
+    public static List<String> getPublicPaths(){
+        return accessPaths.get("ANYONE");
+    }
+}
