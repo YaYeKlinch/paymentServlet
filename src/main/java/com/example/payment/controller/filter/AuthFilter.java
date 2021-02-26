a32f3ec5452de274ea440b530c142a7d68ffd885
YaYeKlinch <klisandrij@gmail.com>
2021-02-26 20:54:09 +0200

    authentification added

--- src/main/java/com/example/payment/controller/filter/AuthFilter.java
+++ src/main/java/com/example/payment/controller/filter/AuthFilter.java
@@ -2,14 +2,28 @@ package com.example.payment.controller.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "authFilter")
+@WebFilter(urlPatterns = {"/accounts/*","/user-payment" , "/make-payment" , "/add-payment"})
 public class AuthFilter implements Filter {
 
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+        if ( session==null || session.getAttribute("LoggedUser")==null){
+            response.sendRedirect("/login-page");
+        }
+        if(response.isCommitted()){
+            return;
+        }
+
+        filterChain.doFilter(request, response);
 
 
     }
